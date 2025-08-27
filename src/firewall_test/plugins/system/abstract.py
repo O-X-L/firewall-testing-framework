41712de30f0b0b9d396cb84a6a5ead64020339d7
Rath Pascal <rath@oxl.at>
2025-08-27 18:42:39 +0200

    added logic to drop bogons-traffic on wan

--- src/firewall_test/plugins/system/abstract.py
+++ src/firewall_test/plugins/system/abstract.py
@@ -8,11 +8,43 @@ class FirewallSystem(ABC):
     # the system has routing-rules for static routing (source-based routing)
     ROUTE_STATIC_RULES = False
 
-    # the system has additional dynamic routes that are queried
-    ROUTE_RUNTIME = False
+    # if the system allows traffic to bogon-networks to be sent to wan/default-route
+    FIREWALL_WAN_DROP_BOGONS = True
 
     # the firewall supports bsd-pf-style quick/lazy matching
     FIREWALL_LAZY_MATCHING = False
 
     # the firewall supports connection-tracking (ct-state)
     FIREWALL_CT = True
+
+    # input = src is remote & dst is local
+    # output = src is local
+    # forward = src is remote & dst is remote
+    FIREWALL_HOOKS = {
+        'input': [],
+        'output': [],
+        'forward': [],
+    }
+
+    # last chain before we need to perform the routing-lookup
+    FIREWALL_PRE_ROUTING = {
+        'input': {},
+        'output': {},
+        'forward': {},
+    }
+
+    # chains where NAT-operations are performed
+    FIREWALL_NAT = {
+        'input': {
+            'dnat': {},
+            'snat': {},
+        },
+        'output': {
+            'dnat': {},
+            'snat': {},
+        },
+        'forward': {
+            'dnat': {},
+            'snat': {},
+        },
+    }
--- src/firewall_test/plugins/system/linux_netfilter.py
+++ src/firewall_test/plugins/system/linux_netfilter.py
@@ -5,8 +5,30 @@ class SystemLinuxNetfilter(FirewallSystem):
     ROUTE_STATIC = True
     ROUTE_STATIC_RULES = True
 
-    # the runtime routes are already included in the 'static'
-    ROUTE_RUNTIME = False
-
     FIREWALL_LAZY_MATCHING = False
     FIREWALL_CT = True
+
+    # see: https://wiki.nftables.org/wiki-nftables/index.php/Netfilter_hooks & https://people.netfilter.org/pablo/nf-hooks.png
+    FIREWALL_HOOKS = {
+        'input': ['ingress', 'prerouting', 'input'],
+        'forward': ['ingress', 'prerouting', 'forward', 'postrouting', 'egress'],
+        'output': ['output', 'postrouting', 'egress'],
+    }
+    FIREWALL_PRE_ROUTING = {
+        'input': {'hook': 'prerouting', 'type': 'nat', 'priority': -100},
+        'forward': {'hook': 'prerouting', 'type': 'nat', 'priority': -100},
+        'output': {'hook': 'output', 'type': 'nat', 'priority': -100},
+    }
+    FIREWALL_NAT = {
+        'input': {
+            'dnat': FIREWALL_PRE_ROUTING['input'],
+        },
+        'forward': {
+            'dnat': FIREWALL_PRE_ROUTING['forward'],
+            'snat': {'hook': 'postrouting', 'type': 'nat', 'priority': 100},
+        },
+        'output': {
+            'dnat': FIREWALL_PRE_ROUTING['output'],
+            'snat': {'hook': 'postrouting', 'type': 'nat', 'priority': 100},
+        },
+    }
